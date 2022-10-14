1d8092ed9565c949c3dd82d3967e62530c792443
Austin Wang <yyaustin@bu.edu>
2022-10-14 03:24:27 -0400

    create: form and list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState } from "react"
 import Todo from "./components/Todo"
 
-const App = ({ todos }) => {
+const App = (props) => {
+    const [todos, setTodos] = useState(props.todos)
+    const [newTodoContent, setNewTodoContent] = useState("")
+
+    const addTodo = (event) => {
+        event.preventDefault()
+        const newTodoObject = {
+            content: newTodoContent,
+            date: new Date().toISOString(),
+            completed: false,
+            id: todos.length + 1,
+        }
+        setTodos(todos.concat(newTodoObject))
+        setNewTodoContent("")
+    }
+
+    const handleNewTodoChange = (event) => setNewTodoContent(event.target.value)
+
     return (
         <div>
             <h1>Todo List</h1>
+            <form onSubmit={addTodo}>
+                <input value={newTodoContent} onChange={handleNewTodoChange} />
+                <button type="submit">Add</button>
+            </form>
             <ul>
                 {todos.map((todo) => (
                     <Todo key={todo.id} todo={todo} />
