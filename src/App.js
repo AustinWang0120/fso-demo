8b4554c0707becae87cc81d76e934c406e1a0be1
Austin Wang <yyaustin@bu.edu>
2022-10-14 02:58:55 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const History = ({ allClicks }) => {
+    if (allClicks.length === 0) {
+        return <div>no reset</div>
+    }
+
+    return <div>{allClicks.join(" ")}</div>
 }
 
-export default App;
+const App = () => {
+    const [counter, setCounter] = useState(0)
+    const [allClicks, setAllClicks] = useState([])
+
+    const handleIncrease = () => setCounter(counter + 1)
+    const handleDecrease = () => setCounter(counter - 1)
+    const handleReset = () => {
+        setCounter(0)
+        setAllClicks(allClicks.concat("reset"))
+    }
+    const handleHello = (name) => () => console.log("hello", name)
+
+    return (
+        <div>
+            <Display counter={counter} />
+            <Button onClick={handleIncrease} text="plus" />
+            <Button onClick={handleDecrease} text="minus" />
+            <Button onClick={handleReset} text="reset" />
+            <Button onClick={handleHello("austin")} text="austin" />
+            <Button onClick={handleHello("potato")} text="potato" />
+            <History allClicks={allClicks} />
+        </div>
+    )
+}
+
+export default App
